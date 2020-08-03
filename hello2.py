e9d027bb8e2666df5f987a72135793f456bcc2bd
Samarth Gowda <samarthgowda@Samarths-MacBook-Pro.local>
2020-08-03 19:09:18 -0400

    init

---
+++ hello2.py
@@ -0,0 +1,30 @@
+from math import *
+phrase = "girl"
+print("fuckyou"+ phrase)
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len((phrase)))
+print(phrase[2])
+print(phrase.index("l"))
+print(phrase.replace("girl", "boy"))
+num = 5
+print(str(num)+"is five") #use str() if you're concat to a str
+print(pow(10,2))
+print(abs(-99))
+print(min(10,6))
+print(max(10,6))
+print(round(10.6))
+print(floor(5.9))
+print(ceil(5.9))
+print(sqrt(5.9))
+name = input("enter your no:")
+print(name)
+num1 = 5
+num2 = 10
+z = num1+num2+ int(name)
+print(z)
+friends =["saeket","true","2"]
+print(friends)
+
+s=['a', 'b', 'c'] + [1, 2, 3]
+print(x)
